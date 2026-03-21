48fd82fb06b8484c59350d633bec676217860945
Michael Fluharty <michael@fluharty.com>
2026-03-21 12:25:24 -0500

    Initial Commit

---
+++ Watchkeeper's StandingTests/Watchkeeper_s_StandingTests.swift
@@ -0,0 +1,16 @@
+//
+//  Watchkeeper_s_StandingTests.swift
+//  Watchkeeper's StandingTests
+//
+//  Created by Michael Fluharty on 3/21/26.
+//
+
+import Testing
+
+struct Watchkeeper_s_StandingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ Watchkeeper's StandingUITests/Watchkeeper_s_StandingUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Watchkeeper_s_StandingUITestsLaunchTests.swift
+//  Watchkeeper's StandingUITests
+//
+//  Created by Michael Fluharty on 3/21/26.
+//
+
+import XCTest
+
+final class Watchkeeper_s_StandingUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
